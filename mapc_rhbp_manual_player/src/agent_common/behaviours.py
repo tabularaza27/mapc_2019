e8fa4430406462f555971ad062c96d99948cba02
Alessandro <alessandro.amic@gmail.com>
2019-05-30 13:39:20 +0200

    aggiornamento manual player

--- mapc_rhbp_manual_player/src/agent_common/behaviours.py
+++ mapc_rhbp_manual_player/src/agent_common/behaviours.py
@@ -16,6 +16,13 @@ from geometry_msgs.msg import Twist
 
 direzione = 'n'
 action = "move"
+task_number = "task0"
+directions=['n','s','w','e']
+fixed_direction = 0
+
+def update_direction():
+    global fixed_direction
+    fixed_direction = (fixed_direction+1)%4
 
 def action_generic_simple(publisher, action_type, params=[]):
     """
@@ -29,11 +36,30 @@ def action_generic_simple(publisher, action_type, params=[]):
     action.params = params
     publisher.publish(action)
 
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
+
 def callback_direzioni(data):
     global direzione
     global action
-
-    if (data.data == "n" or data.data == "s" or data.data == "e" or data.data == "w"):
+    global task_number
+    if is_number(data.data):
+        task_number = "task" + data.data
+        rospy.loginfo("task updated to " + task_number)
+    elif (data.data == "n" or data.data == "s" or data.data == "e" or data.data == "w"):
         direzione = data.data
         action = "move"
         rospy.loginfo("MOVE | new direction: " + direzione)
@@ -46,7 +72,7 @@ def callback_direzioni(data):
     elif (data.data == "z"):
         action = "detach"
         rospy.loginfo("detaching")
-    elif (data.data == "s"):
+    elif (data.data == "t"):
         action = "submit"
         rospy.loginfo("submitting")
     elif (data.data == "l"):
@@ -58,7 +84,9 @@ def callback_direzioni(data):
     elif (data.data == "c"):
         action = "connect"
         rospy.loginfo("connecting")
-
+    elif (data.data == "g"):
+        update_direction()
+        rospy.loginfo("changing fixed direction to " + directions[fixed_direction])
 class GenericActionBehaviour(BehaviourBase):
     """
     A simple behaviour for triggering generic MAPC actions that just need a action type and static parameters
@@ -107,23 +135,24 @@ class ManualMove(BehaviourBase):
                                                    , queue_size=10)
 
         #rospy.Subscriber("/directions", String, callback_direzioni)
-        rospy.Subscriber("/key_player", String, callback_direzioni)
+        rospy.Subscriber("/key_player_"+self._agent_name, String, callback_direzioni)
 
     def do_step(self):
+        if self._perception_provider._request_action_msg.agent.last_action != 'no_action':
+            rospy.loginfo("Current perception:\n" +
+                          "\tRaw Perception: " + str(self._perception_provider._request_action_msg.agent))
         global action
         if (action == "move"):
             params = [KeyValue(key="direction", value=direzione)]
-            rospy.loginfo(self._agent_name + "::" + self._name + " executing move to " + str(params) + "\n" + 
-                        "Current perception:\n" +
-                        "\tDispensers: " + str(self._perception_provider.dispensers)+"\n"+
-                        "\tObstacles: " + str(self._perception_provider.obstacles)+"\n"+
-                        "\tGoals: " + str(self._perception_provider.goals))
+
+            rospy.loginfo(self._agent_name + "::" + self._name + " executing move to " + str(params) + "\n")
+
             action_generic_simple(publisher=self._pub_generic_action, action_type=GenericAction.ACTION_TYPE_MOVE, params=params)
             action = "wait"
         elif (action == "dispense"):
             #random_move = ['n', 's', 'e', 'w']
             donde = 'n'
-            params = [KeyValue(key="direction", value=donde)]
+            params = [KeyValue(key="direction", value=directions[fixed_direction])]
 
             rospy.loginfo(self._agent_name + "::" + self._name + " request dispense " + str(params) + "\n" + 
                         "Current perception:\n" +
@@ -137,7 +166,7 @@ class ManualMove(BehaviourBase):
         elif (action == "attach"):
             #random_move = ['n', 's', 'e', 'w']
             donde = 'n'
-            params = [KeyValue(key="direction", value=donde)]
+            params = [KeyValue(key="direction", value=directions[fixed_direction])]
 
             rospy.loginfo(self._agent_name + "::" + self._name + " request attach " + str(params))
             action_generic_simple(publisher=self._pub_generic_action, action_type=GenericAction.ACTION_TYPE_ATTACH,
@@ -146,10 +175,21 @@ class ManualMove(BehaviourBase):
             action = "wait"
         elif (action == "connect"):
             #random_move = ['n', 's', 'e', 'w']
-            donde = 'TO DOOOOOOO'
-            params = [KeyValue(key="direction", value=donde)]
+            if self._agent_name == 'agentA2':
+                agent_connect = 'agentA1'
+                x = '1'
+                y = '0'
+            else:
+                agent_connect = 'agentA2'
+                x = '-1'
+                y = '0'
+
+            params = [KeyValue(key="agent", value=agent_connect),
+                      KeyValue(key="x", value=x),
+                      KeyValue(key="y", value=y)]
 
             rospy.loginfo(self._agent_name + "::" + self._name + " request connect " + str(params))
+
             action_generic_simple(publisher=self._pub_generic_action, action_type=GenericAction.ACTION_TYPE_CONNECT,
                                   params=params)
 
@@ -157,7 +197,7 @@ class ManualMove(BehaviourBase):
         elif (action == "detach"):
             #random_move = ['n', 's', 'e', 'w']
             donde = 'n'
-            params = [KeyValue(key="direction", value=donde)]
+            params = [KeyValue(key="direction", value=directions[fixed_direction])]
 
             rospy.loginfo(self._agent_name + "::" + self._name + " request detach " + str(params))
             action_generic_simple(publisher=self._pub_generic_action, action_type=GenericAction.ACTION_TYPE_DETACH,
@@ -166,8 +206,14 @@ class ManualMove(BehaviourBase):
             action = "wait"
         elif (action == "submit"):
             #random_move = ['n', 's', 'e', 'w']
-            donde = 1
-            params = [KeyValue(key="task", value=donde)]
+            rospy.loginfo("Current perception:\n" +
+                          "\tDispensers: " + str(self._perception_provider.dispensers) + "\n" +
+                          "\tObstacles: " + str(self._perception_provider.obstacles) + "\n" +
+                          "\tGoals: " + str(self._perception_provider.goals) + "\n" +
+                          "\tRaw Perception: " + str(self._perception_provider._request_action_msg.agent))
+
+
+            params = [KeyValue(key="task", value=task_number)]
 
             rospy.loginfo(self._agent_name + "::" + self._name + " request submit " + str(params))
             action_generic_simple(publisher=self._pub_generic_action, action_type=GenericAction.ACTION_TYPE_SUBMIT,
